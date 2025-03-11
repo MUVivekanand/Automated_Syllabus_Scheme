879cb52b6df7cc212187eb56a4b1e72f2768571e
MUVivekanand <121551571+MUVivekanand@users.noreply.github.com>
2025-03-11 14:49:15 +0530

    fixed issues part-2

--- src/backend/components/SemInfo/seminfo.route.js
+++ src/backend/components/SemInfo/seminfo.route.js
@@ -2,14 +2,12 @@ const express = require("express");
 
 const{
     updateSemInfo,
-    updateCredits,
     getSemInfo,
 } = require("./seminfo.controller")
 
 const router = express.Router();
 
 router.post("/updateSemInfo", updateSemInfo);
-router.post("/updateCredits", updateCredits);
 
 router.get("/getSemInfo", getSemInfo);
 
